07c15faeb4067d51c9152ab5a11b13ab8a1c848f
stackup-dev <hello@stackup.dev>
2023-04-12 11:48:20 +0900

    add mirrorNodeClient.js

---
+++ src/mirrorNodeClient.js
@@ -0,0 +1,16 @@
+export class MirrorNodeClient {
+  url = "https://mainnet-public.mirrornode.hedera.com/";
+  constructor(network) {
+    if (network === "testnet") {
+      this.url = "https://testnet.mirrornode.hedera.com/";
+    } else if (network === "previewnet") {
+      this.url = "https://previewnet.mirrornode.hedera.com/";
+    }
+  }
+
+  async getAccountInfo(accountId) {
+    const accountInfo = await fetch(`${this.url}api/v1/accounts/` + accountId, { method: "GET" });
+    const accountInfoJson = await accountInfo.json();
+    return accountInfoJson;
+  }
+}
